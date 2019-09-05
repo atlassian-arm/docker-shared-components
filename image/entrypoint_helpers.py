728cdb355f1ee9c5765a9b0d54e4fbfec35288d0
Dave Chevell <dchevell@atlassian.com>
2019-09-05 12:32:17 +1000

    run set_perms recursively

--- image/entrypoint_helpers.py
+++ image/entrypoint_helpers.py
@@ -31,8 +31,12 @@ jenv = j2.Environment(
 # Utils
 
 def set_perms(path, user, group, mode):
-    shutil.chown(path, user=user, group=group)
-    os.chmod(path, mode)
+    for dirpath, dirnames, filenames in os.walk(path):
+        shutil.chown(dirpath, user=user, group=group)
+        os.chmod(dirpath, mode)
+        for filename in filenames:
+            shutil.chown(os.path.join(dirpath, filename), user=user, group=group)
+            os.chmod(os.path.join(dirpath, filename), mode)
 
 def gen_cfg(tmpl, target, user='root', group='root', mode=0o644, overwrite=True):
     if not overwrite and os.path.exists(target):
