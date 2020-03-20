6b14bf5d08c0ce82a9591caa3de4c7c6560a2218
Dave Chevell <dchevell@atlassian.com>
2020-03-20 20:49:40 +1100

    Updated push-readme.py to address Docker Hub breaking changes

--- image/push-readme.py
+++ image/push-readme.py
@@ -23,10 +23,11 @@ with open(README_FILE) as f:
     full_description = f.read()
 data = {'registry': 'registry-1.docker.io', 'full_description': full_description}
 headers = {'Authorization': f'JWT {docker_token}'}
-r = requests.patch(f'https://cloud.docker.com/v2/repositories/{DOCKER_REPO}/',
+r = requests.patch(f'https://hub.docker.com/v2/repositories/{DOCKER_REPO}/',
                    json=data, headers=headers)
 
 if r.status_code == requests.codes.ok:
     logging.info(f'Successfully updated {README_FILE} for {DOCKER_REPO}')
 else:
     logging.info(f'Unable to update {README_FILE} for {DOCKER_REPO}, response code: {r.status_code}')
+    r.raise_for_status()
