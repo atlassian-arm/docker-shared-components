e4e85f9b72faceb303d69c828fe6af8790b38e18
Steve Smith <ssmith@atlassian.com>
2021-08-24 12:09:41 +1000

    DCD-1350: Add wrapper to send a signal to the applcation.

---
+++ support/send-sig.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+SIG=$1
+WAIT=$2
+SHDIR=$(dirname $0)
+
+source ${SHDIR}/common.sh
+
+kill -$SIG $APP_PID
+
+if [[ "$WAIT" != "wait" ]]; then
+    exit 0
+fi
+
+while [[ -e /proc/$APP_PID ]]; do
+    echo wait
+    /bin/sleep 0.5
+done
